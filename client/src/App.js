3405e2f17299723786c95279ad1a9b684ac48d92
John Rodler <john@jrod.io>
2018-01-04 20:03:43 -0800

    add navbar

--- client/src/App.js
+++ client/src/App.js
@@ -40,6 +40,44 @@ class App extends Component {
 
     return (
       <div className="App">
+        {/*Navbar*/}
+        <nav className="navbar navbar-expand-lg navbar-light aqua-gradient">
+          {/* Navbar brand */}
+          <a className="navbar-brand" href="">Navbar</a>
+          {/* Collapse button */}
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
+          {/* Collapsible content */}
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              {/* Links */}
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                  <a className="nav-link" href="">Home <span className="sr-only">(current)</span></a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="">Features</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="">Pricing</a>
+                </li>
+                {/* Dropdown */}
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                  <div className="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+                    <a className="dropdown-item" href="">Action</a>
+                    <a className="dropdown-item" href="">Another action</a>
+                    <a className="dropdown-item" href="">Something else here</a>
+                  </div>
+                </li>
+              </ul>
+              {/* Links */}
+              {/* Search form */}
+              <form className="form-inline">
+                  <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
+              </form>
+          </div>
+          {/* Collapsible content */}
+        </nav>
+        {/*Navbar*/}
         <div className="container">
           <div className="row">
             <div className="col">1</div>
