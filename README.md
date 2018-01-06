cf0064c9ca1fb9db35f1f3e0971ed952db3d782b
John Rodler <john@jrod.io>
2018-01-06 14:21:11 -0800

    setup basic component testing w/ enzyme + chai

--- README.md
+++ README.md
@@ -3,9 +3,17 @@ Client Thank You Card Manager at LSVT.
 
 ## To Do
 * setup mongoDB, mongoose schema + express routes
-* stub tests using fetch (fixes App.test.js)
+* setup add client + employee modals
+* setup editable tables component
+* setup fetch error handling
+* setup stubbed sub-component fetch tests
+* setup import spreadsheet data feature
+* setup matching + timing logic
+* setup mailer system
+* setup create history
 
 ## Complete
+* setup basic component testing w/ enzyme + chai
 * setup base components :+1:
 * setup nav + layout :+1:
 * setup Sass :+1:
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import { assert } from 'chai';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it('renders the <App /> without crashing', () => {
+  shallow(<App />);
+});
+
+it('should render a <Nav /> component', () => {
+  const app = shallow(<App />);
+  const nav = app.find('Nav');
+
+  assert.equal(nav.length, 1);
+});
+
+it('should render a <Clients /> component', () => {
+  const app = shallow(<App />);
+  const clients = app.find('Clients');
+
+  assert.equal(clients.length, 1);
+});
+
+it('should render a <Employees /> component', () => {
+  const app = shallow(<App />);
+  const employees = app.find('Employees');
+
+  assert.equal(employees.length, 1);
+});
+
+it('should render a <History /> component', () => {
+  const app = shallow(<App />);
+  const history = app.find('History');
+
+  assert.equal(history.length, 1);
 });
